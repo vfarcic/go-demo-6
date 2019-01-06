09d3175c4869daaa74b8e7b6a2fdd6959dcbe969
Viktor Farcic <vfarcic@farcic.com>
2019-01-06 17:08:28 +0100

    This is a PR

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 		delayNum, _ := strconv.Atoi(delay)
 		sleep(time.Duration(delayNum) * time.Millisecond)
 	}
-	io.WriteString(w, "hello, world!\n")
+	io.WriteString(w, "hello, PR!\n")
 }
 
 func RandomErrorServer(w http.ResponseWriter, req *http.Request) {

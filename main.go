5e6b02239de41850be2725a7a6e92d58bbd81652
Viktor Farcic <vfarcic@farcic.com>
2019-08-06 20:22:26 +0200

    Recreate strategy

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 		delayNum, _ := strconv.Atoi(delay)
 		sleep(time.Duration(delayNum) * time.Millisecond)
 	}
-	io.WriteString(w, "hello, recreate!\n")
+	io.WriteString(w, "hello, rolling update!\n")
 }
 
 func RandomErrorServer(w http.ResponseWriter, req *http.Request) {

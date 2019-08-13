6fb4e61f45c0ee7b75c2434f3cbe19d0e4961ad1
Robert Palethorpe <robert.palethorpe@dimensiondata.com>
2019-08-14 05:19:07 +1000

    Testing

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 		delayNum, _ := strconv.Atoi(delay)
 		sleep(time.Duration(delayNum) * time.Millisecond)
 	}
-	io.WriteString(w, "hello, devpod with tests!\n")
+	io.WriteString(w, "hello, robpod with tests!\n")
 }
 
 func RandomErrorServer(w http.ResponseWriter, req *http.Request) {

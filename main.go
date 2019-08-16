e273177a95880927251b106f7e64525f8ffcce73
Viktor Farcic <vfarcic@farcic.com>
2019-08-16 10:41:14 -0700

    Added progressive deployment

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 		delayNum, _ := strconv.Atoi(delay)
 		sleep(time.Duration(delayNum) * time.Millisecond)
 	}
-	io.WriteString(w, "hello, progressive!\n")
+	io.WriteString(w, "hello, no one!\n")
 }
 
 func RandomErrorServer(w http.ResponseWriter, req *http.Request) {

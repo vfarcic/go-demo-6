79b6a6ddf28fe7ea36edcc9e6534a4073f276a6c
Viktor Farcic <vfarcic@farcic.com>
2019-08-16 17:16:50 -0700

    Added progressive deployment

--- main_test.go
+++ main_test.go
@@ -66,7 +66,7 @@ func (s *MainTestSuite) Test_HelloServer_WritesOk() {
 		RandomErrorServer(w, req)
 	}
 
-	w.AssertCalled(s.T(), "Write", []byte("Everything is still OK\n"))
+	w.AssertCalled(s.T(), "Write", []byte("Everything is still OK with progressive delivery\n"))
 }
 
 func (s *MainTestSuite) Test_HelloServer_WritesNokEventually() {
